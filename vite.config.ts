1fefef82d959c775e771b061a4e7dad46a55c1c3
thesyedhr <thesyedhrowns@gmail.com>
2026-08-25 17:27:35 +0530

    final

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(({ command }) => {
   return {
-    base: command === 'build' ? '/Project1/' : '/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
